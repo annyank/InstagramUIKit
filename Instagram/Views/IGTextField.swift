376c4d61bf554b4a6a5f2019ddfa0c25322480f4
annyank <hannalobava@gmail.com>
2021-12-16 02:10:23 +0300

    Create basic Sign In logic

---
+++ Instagram/Views/IGTextField.swift
@@ -0,0 +1,20 @@
+//
+//  IGTextField.swift
+//  Instagram
+//
+//  Created by Ann Yank on 16.12.21.
+//
+
+import UIKit
+
+class IGTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Instagram/Views/SignInHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInHeaderView.swift
+//  Instagram
+//
+//  Created by Ann Yank on 15.12.21.
+//
+
+import UIKit
+
+class SignInHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
